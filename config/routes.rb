de34d10e01f6565a3075c77cc61469727f514180
Alexander Weiler <alexander.weiler@gmail.com>
2017-02-02 22:00:12 +0100

    Add cities

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  
-  get '/ui'  => 'ui#index'
-  get '/ui#' => 'ui#index'
+  scope :api, defaults: {format: :json} do
+    resources :cities, except: [:new, :edit]
+    get '/ui'  => 'ui#index'
+    get '/ui#' => 'ui#index'
   root "ui#index"
-
+  end
 end
